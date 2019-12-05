c224bd56e5dcd2b3e72277d201b4236fad84a477
João David <joaodavid512@gmail.com>
2019-12-05 11:18:15 +0000

    client communicating contains message with root

--- Binary_Tree_in_Actors/src/main.erl
+++ Binary_Tree_in_Actors/src/main.erl
@@ -1,30 +1,92 @@
 %% @author PC
 %% @doc @todo Add description to main.
 
-
+%  c(main), main:main().
 -module(main).
 
--export([main/0]).
+-export([main/0,client/1,binary_search/3,contains/5,binary_search_handle_response/3]).
 
 -record(actor_node,{left,right}).
 -record(binary_search,{root=#actor_node{}}).
 
-returnsValue() ->
-	40.
+
 
 main() ->
-	Var = returnsValue(),
-	io:format("The value is: ~p.", [Var]).
+	register(binary_search_root, spawn(main, binary_search, [99,undefined,undefined])),
+	register(client, spawn(main, client, [3])),
+	io:format("Main finished\n", []).
 
-actor_node() ->
+
+client(N) ->
+	io:format("Entering Client\n", []),
+	binary_search_root ! {contains,99},
+	receive
+        Response ->
+            io:format("Client Response: ~p", [Response])
+    end,
+	if 
+      N > 0 ->
+		  client(N-1);
+      true -> 
+         io:format("Finished Client\n", [])
+   end.
+	
+
+
+binary_search(Value, Left, Right) ->
+	io:format("value:~p left:~p right:~p \n", [Value,Left,Right]),
 	receive
         {insert, Value} ->
             io:format("Handling: ~s~n", [Value]),
-            actor_node();
+            binary_search(Value, Left, Right);
 		{delete, Value} ->
             io:format("Handling: ~s~n", [Value]),
-            actor_node();
-		{contains} ->
-            actor_node()
+            binary_search(Value, Left, Right);
+		{contains, ValueToFind} ->
+			io:format("contains?: ~p\n", [ValueToFind]),
+            contains(Value, Left, Right, binary_search, ValueToFind)
+    end,
+	binary_search_handle_response(Value, Left, Right).
+
+binary_search_handle_response(Value, Left, Right) ->
+	io:format("handling response ~p \n", []),
+	receive
+		{contains, ValueToFind, Response} ->
+			io:format("contains?: ~p\n", [ValueToFind]),
+			client ! Response
+    end,
+	binary_search(Value, Left, Right).
+
+actor_node(Value, Left, Right) ->
+	receive
+        {insert, Value} ->
+			1;
+		{delete, Value} ->
+			1;
+		{contains, ValueToFind} ->
+            contains(Value, Left, Right, actor_node, ValueToFind)
     end.
+
+contains(Value, Left, Right, F, ValueToFind) ->
+	case ValueToFind of 
+     	Value ->
+			io:format("inside contains yes, contains: ~p\n", [ValueToFind]),
+			binary_search_root ! {contains, ValueToFind, true};
+      	N when N < Value ->
+			if 
+      		Left == undefined -> 
+        		binary_search_root ! {contains, ValueToFind, false};  
+      		true -> 
+         		Left ! {contains, ValueToFind}
+   			end;
+		N when N > Value ->
+			if 
+      		Right == undefined -> 
+        		binary_search_root ! {contains, ValueToFind, false};
+      		true -> 
+         		Right ! {contains, ValueToFind}
+   			end
+	end.
+	%F(Value, Left, Right).
+
 
