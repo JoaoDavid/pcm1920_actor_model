d0c3462fdfbab1dcc5f13cdbac05a03c32ea2d5b
João David <joaodavid512@gmail.com>
2019-12-07 12:42:32 +0000

    delete working, except when deleting root node

--- Binary_Tree_in_Actors/src/main.erl
+++ Binary_Tree_in_Actors/src/main.erl
@@ -4,7 +4,7 @@
 %  c(main), main:start().
 -module(main).
 
--export([start/0,client/0,client_handle_response/0,binary_search/0,binary_search/1,actor_node/3,insert/4,contains/4]).
+-export([start/0,client/0,client_handle_response/0,binary_search/0,binary_search/1,actor_node/3,insert/4,contains/4, delete/5]).
 
 
 start() ->
@@ -14,14 +14,40 @@ start() ->
 
 client() ->
 	io:format("Entering Client\n", []),
-	binary_tree_api ! {insert,3},
+	binary_tree_api ! {insert,8},
+	binary_tree_api ! {insert,4},
+	binary_tree_api ! {insert,12},
+	binary_tree_api ! {contains,8},
+	binary_tree_api ! {contains,4},
+	binary_tree_api ! {contains,12},
+
 	binary_tree_api ! {insert,2},
-	binary_tree_api ! {contains,3},
-	binary_tree_api ! {insert,1},
-	binary_tree_api ! {insert,3},
-	binary_tree_api ! {contains,3},
-	binary_tree_api ! {contains,1},
+	binary_tree_api ! {insert,6},
+	binary_tree_api ! {insert,10},
+	binary_tree_api ! {insert,14},
+
+	binary_tree_api ! {contains,8},
+	binary_tree_api ! {contains,4},
+	binary_tree_api ! {contains,12},
 	binary_tree_api ! {contains,2},
+	binary_tree_api ! {contains,6},
+	binary_tree_api ! {contains,10},
+	binary_tree_api ! {contains,14},
+
+	binary_tree_api ! {delete,4},
+	binary_tree_api ! {insert,5},
+	binary_tree_api ! {contains,5},
+	binary_tree_api ! {contains,6},
+	binary_tree_api ! {contains,4},
+	binary_tree_api ! {contains,2},
+
+	binary_tree_api ! {delete,10},
+	binary_tree_api ! {insert,10},
+	binary_tree_api ! {contains,13},
+	binary_tree_api ! {contains,10},
+	binary_tree_api ! {contains,14},
+	
+	
 	
 	client_handle_response_forever().
 	
@@ -60,7 +86,7 @@ binary_search(Root) ->
 			Root ! {insert, ValueToInsert};
 		{delete, ValueToDelete} ->
 			%io:format("API: delete ~p\n", [ValueToDelete]),
-			Root ! {delete, ValueToDelete};
+			Root ! {delete, ValueToDelete, self()};
 		{contains, ValueToFind} ->
 			%io:format("API: contains ~p\n", [ValueToFind]),
 			Root ! {contains, ValueToFind}
@@ -69,16 +95,88 @@ binary_search(Root) ->
 	binary_search(Root).
 
 actor_node(Value, Left, Right) ->
+	%Father whose child died
 	receive
         {insert, ValueToInsert} ->
-            %io:format("Node Insert: ~p\n", [ValueToInsert]),
 			insert(Value, Left, Right, ValueToInsert);
-		{delete, ValueToDelete} ->
-            io:format("Node Delete: ~p\n", [ValueToDelete]);
+		{delete, ValueToDelete, Father} ->
+			delete(Value, Left, Right, ValueToDelete, Father);
 		{contains, ValueToFind} ->
-			%io:format("Node Contains: ~p\n", [ValueToFind]),
-            contains(Value, Left, Right, ValueToFind)
+            contains(Value, Left, Right, ValueToFind);
+		{die, Father} ->			
+            Father ! {reinsert, Value};
+		{reinsert, ValueToReInserted} ->			
+            reinsert(Value, Left, Right, ValueToReInserted);
+		{child_died, Pid} ->			
+            if
+				Pid == Left ->
+					actor_node(Value, undefined, Right);
+				true ->
+					actor_node(Value, Left, undefined)
+			end
     end.
+
+
+delete(Value, Left, Right, ValueToDelete, Father) ->
+	case ValueToDelete of 
+     	Value ->
+			Father ! {child_died, self()},
+			if
+				Left /= undefined ->
+					Left ! {die, Father};
+				true ->
+					ok
+			end,
+			if
+				Right /= undefined ->
+					Right ! {die, Father};
+				true ->
+					ok
+			end,			
+			binary_tree_api ! {delete, ValueToDelete, true};	
+      	N when N < Value ->
+			if 
+      		Left == undefined ->
+        		binary_tree_api ! {delete, ValueToDelete, does_not_exist},
+				actor_node(Value, Left, Right);
+      		true -> 
+         		Left ! {delete, ValueToDelete, self()},
+				actor_node(Value, Left, Right)
+   			end;
+		N when N > Value ->
+			if 
+      		Right == undefined -> 
+        		binary_tree_api ! {delete, ValueToDelete, does_not_exist},
+				actor_node(Value, Left, Right);
+      		true -> 
+         		Right ! {delete, ValueToDelete, self()},
+				actor_node(Value, Left, Right)
+   			end
+	end.
+
+reinsert(Value, Left, Right, ValueToInsert) ->
+	case ValueToInsert of 
+     	Value ->
+			actor_node(Value, Left, Right);
+      	N when N < Value ->
+			if 
+      		Left == undefined ->
+				NewNodePid = spawn(main, actor_node, [ValueToInsert,undefined,undefined]),
+				actor_node(Value, NewNodePid, Right);
+      		true -> 
+         		Left ! {reinsert, ValueToInsert},
+				actor_node(Value, Left, Right)
+   			end;
+		N when N > Value ->
+			if 
+      		Right == undefined -> 
+        		NewNodePid = spawn(main, actor_node, [ValueToInsert,undefined,undefined]),
+				actor_node(Value, Left, NewNodePid);
+      		true -> 
+         		Right ! {reinsert, ValueToInsert},
+				actor_node(Value, Left, Right)
+   			end
+	end.
 
 insert(Value, Left, Right, ValueToInsert) ->
 	case ValueToInsert of 
