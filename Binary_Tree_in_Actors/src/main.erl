6fec8e4b219be7fd0363a6bbbb837f4b390bb6ab
João David <joaodavid512@gmail.com>
2019-12-07 00:44:37 +0000

    contains and insert working

--- Binary_Tree_in_Actors/src/main.erl
+++ Binary_Tree_in_Actors/src/main.erl
@@ -1,28 +1,33 @@
 %% @author PC
 %% @doc @todo Add description to main.
 
-%  c(main), main:main().
+%  c(main), main:start().
 -module(main).
 
--export([main/0,client/0,client_handle_response/0,binary_search/0,binary_search/1,actor_node/3,insert/4,contains/4]).
+-export([start/0,client/0,client_handle_response/0,binary_search/0,binary_search/1,actor_node/3,insert/4,contains/4]).
 
 
-main() ->
+start() ->
 	register(binary_tree_api, spawn(main, binary_search, [])),
 	register(client, spawn(main, client, [])).
 
 
 client() ->
 	io:format("Entering Client\n", []),
+	binary_tree_api ! {insert,3},
+	binary_tree_api ! {insert,2},
 	binary_tree_api ! {contains,3},
-	client_handle_response(),
-	binary_tree_api ! {contains,1},
-	client_handle_response(),
 	binary_tree_api ! {insert,1},
-	client_handle_response(),
+	binary_tree_api ! {insert,3},
+	binary_tree_api ! {contains,3},
 	binary_tree_api ! {contains,1},
-	client_handle_response().
-
+	binary_tree_api ! {contains,2},
+	
+	client_handle_response_forever().
+	
+client_handle_response_forever() ->
+	client_handle_response(),
+	client_handle_response_forever().
 
 client_handle_response() ->
 	receive
@@ -41,7 +46,7 @@ binary_search() ->
         {insert, ValueToInsert} ->
 			Root = spawn(main, actor_node, [ValueToInsert,undefined,undefined]),
 			client ! {insert,ValueToInsert,true},
-			io:format("Root pid: ~p\n", [Root]),
+			%io:format("Root pid: ~p\n", [Root]),
 			binary_search(Root);
 		{Op, Value} ->
 			client ! {Op,Value,false},
@@ -49,16 +54,15 @@ binary_search() ->
 	end.
 			
 binary_search(Root) ->
-	io:format("Entrei no binary_search root\n", []),
 	receive
         {insert, ValueToInsert} ->
-			io:format("API: insert ~p\n", [ValueToInsert]),
+			%io:format("API: insert ~p\n", [ValueToInsert]),
 			Root ! {insert, ValueToInsert};
 		{delete, ValueToDelete} ->
-			io:format("API: delete ~p\n", [ValueToDelete]),
+			%io:format("API: delete ~p\n", [ValueToDelete]),
 			Root ! {delete, ValueToDelete};
 		{contains, ValueToFind} ->
-			io:format("API: contains ~p\n", [ValueToFind]),
+			%io:format("API: contains ~p\n", [ValueToFind]),
 			Root ! {contains, ValueToFind}
     end,
 	binary_search_handle_response(),
@@ -67,20 +71,20 @@ binary_search(Root) ->
 actor_node(Value, Left, Right) ->
 	receive
         {insert, ValueToInsert} ->
-            io:format("Node Insert: ~p\n", [ValueToInsert]),
+            %io:format("Node Insert: ~p\n", [ValueToInsert]),
 			insert(Value, Left, Right, ValueToInsert);
 		{delete, ValueToDelete} ->
             io:format("Node Delete: ~p\n", [ValueToDelete]);
 		{contains, ValueToFind} ->
-			io:format("Node Contains: ~p\n", [ValueToFind]),
+			%io:format("Node Contains: ~p\n", [ValueToFind]),
             contains(Value, Left, Right, ValueToFind)
     end.
 
 insert(Value, Left, Right, ValueToInsert) ->
 	case ValueToInsert of 
      	Value ->
-			io:format("inside insert already exists: ~p\n", [ValueToInsert]),
-			binary_tree_api ! {insert, ValueToInsert, already_exists};
+			binary_tree_api ! {insert, ValueToInsert, already_exists},
+			actor_node(Value, Left, Right);
       	N when N < Value ->
 			if 
       		Left == undefined ->
@@ -106,7 +110,6 @@ insert(Value, Left, Right, ValueToInsert) ->
 contains(Value, Left, Right, ValueToFind) ->
 	case ValueToFind of 
      	Value ->
-			io:format("inside contains yes, contains: ~p\n", [ValueToFind]),
 			binary_tree_api ! {contains, ValueToFind, true};
       	N when N < Value ->
 			if 
@@ -122,6 +125,7 @@ contains(Value, Left, Right, ValueToFind) ->
       		true -> 
          		Right ! {contains, ValueToFind}
    			end
-	end.
+	end,
+	actor_node(Value, Left, Right).
 
 
