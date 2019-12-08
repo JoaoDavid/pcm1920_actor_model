e9c39c981d6440e3bcd0f0350939583d289758aa
João David <joaodavid512@gmail.com>
2019-12-08 19:17:44 +0000

    implemented destroy message, that destroys all actors that compose the tree

--- Binary_Tree_in_Actors/src/main.erl
+++ Binary_Tree_in_Actors/src/main.erl
@@ -1,5 +1,4 @@
-%% @author PC
-%% @doc @todo Add description to main.
+%% @author Joao David n49448
 
 %  c(main), main:start().
 -module(main).
@@ -9,7 +8,8 @@
 
 start() ->
 	register(binary_tree_api, spawn(main, binary_search, [])),
-	register(client, spawn(main, client, [])).
+	register(client, spawn(main, client, [])),
+	ok.
 
 
 client() ->
@@ -53,18 +53,22 @@ client() ->
 	binary_tree_api ! {insert,8},
 	binary_tree_api ! {contains,8},
 	
+	binary_tree_api ! {destroy},
+	client_handle_response().
 	
-	client_handle_response_forever().
-	
-client_handle_response_forever() ->
-	client_handle_response(),
-	client_handle_response_forever().
+
 
 client_handle_response() ->
+	Timeout = 6000,
 	receive
         {Op, Value, Response} ->
-            io:format("Client Response: ~p ~p ~p\n", [Op, Value, Response])
+            io:format("Client Response: ~p ~p ~p\n", [Op, Value, Response]),
+			client_handle_response()
+	after
+    	Timeout ->
+      		io:format("Client ending after ~p miliseconds without new messages\n", [Timeout])
     end.
+
 
 binary_search_handle_response() -> 
 	receive
@@ -78,22 +82,30 @@ binary_search() ->
 			Root = spawn(main, actor_node, [ValueToInsert,undefined,undefined,self(),true]),
 			client ! {insert,ValueToInsert,true},
 			binary_search(Root);
+		{destroy} ->
+			binary_search_destroyed;
 		{Op, Value} ->
-			client ! {Op,Value,false},
+			client ! {Op,Value,does_not_exist},
 			binary_search()
 	end.
 			
 binary_search(Root) ->
 	receive
         {insert, ValueToInsert} ->
-			Root ! {insert, ValueToInsert};
+			Root ! {insert, ValueToInsert},
+			binary_search_handle_response(),
+			binary_search(Root);
 		{delete, ValueToDelete} ->
-			Root ! {delete, ValueToDelete};
+			Root ! {delete, ValueToDelete},
+			binary_search_handle_response(),
+			binary_search(Root);
 		{contains, ValueToFind} ->
-			Root ! {contains, ValueToFind}
-    end,
-	binary_search_handle_response(),
-	binary_search(Root).
+			Root ! {contains, ValueToFind},
+			binary_search_handle_response(),
+			binary_search(Root);
+		{destroy} ->
+			Root ! {destroy}		
+    end.
 
 actor_node(Value, Left, Right, Father, State) ->
 	receive
@@ -113,8 +125,24 @@ actor_node(Value, Left, Right, Father, State) ->
 					actor_node(Value, undefined, Right, Father, State);
 				Pid == Right ->
 					actor_node(Value, Left, undefined, Father, State)
-			end
+			end;
+		{destroy} ->
+			actor_node_destroy(Left, Right)
     end.
+
+actor_node_destroy(Left, Right) ->
+	if
+		Left /= undefined ->
+			Left ! {destroy};
+		true ->
+			no_left_child
+	end,
+	if
+		Right /= undefined ->
+			Right ! {destroy};
+		true ->
+			no_right_child
+	end.
 
 
 delete(Value, Left, Right, ValueToDelete, Father, State) ->
@@ -127,12 +155,13 @@ delete(Value, Left, Right, ValueToDelete, Father, State) ->
 						State == true ->							
 							binary_tree_api ! {delete, ValueToDelete, true},
 							actor_node(Value, Left, Right, Father, false);
-						State /= false ->
+						State == false ->
 							binary_tree_api ! {delete, ValueToDelete, does_not_exist},
 							actor_node(Value, Left, Right, Father, State)
 					end;
 				PidInterface /= Father ->
-					Father ! {child_died, self()}
+					Father ! {child_died, self()},
+					binary_tree_api ! {delete, ValueToDelete, true}
 			end,			
 			if
 				Left /= undefined ->
@@ -145,8 +174,7 @@ delete(Value, Left, Right, ValueToDelete, Father, State) ->
 					Right ! {reincarnate, Father};
 				true ->
 					ok
-			end,			
-			binary_tree_api ! {delete, ValueToDelete, true};	
+			end;
       	N when N < Value ->
 			if 
       		Left == undefined ->
